84b76449b75d839eb459cb655c2583e0f2d5bd3a
iamswapdev <swapnilv27012016@gmail.com>
2016-11-04 13:28:55 +0530

    login model formated code
    login model formated code

--- application/models/Login_model.php
+++ application/models/Login_model.php
@@ -5,10 +5,12 @@
 			$this->load->database();
 		}
 		public function autho($data){
-			
-		 $query=$this->db->query('select * from admin where username="'.$data['username'].'" and password="'.$data['password'].'"');
-		 $rows = $query->num_rows();
-		 if($rows > 0){
+		 $this->db->where('username',$data['username']);
+		 $this->db->where('password',$data['password']);
+		 $data=$this->db->get('admin');
+		 
+		  $rows = $data->num_rows();
+		 if($rows == 1){
 			 return true;
 		 }else
 		 {
@@ -16,9 +18,11 @@
 		 }
 	    }
 		public function forgot_pass($data){
-		 $query=$this->db->query('select * from admin where email_address="'.$data['email'].'"');
-		 $rows = $query->num_rows();
-		 if($rows > 0){
+		 $this->db->where('email_address',$data['email']);
+		 $data=$this->db->get('admin');
+		 
+		  $rows = $data->num_rows();
+		 if($rows == 1){
 			 return true;
 		 }else
 		 {
