aa7ae25bd5f1c29d32f0534f7868f64b487777d5
iamswapdev <swapnilv27012016@gmail.com>
2016-11-04 11:56:47 +0530

    Added header and logincontroller format
    Added header and logincontroller format

--- application/controllers/Login_controller.php
+++ application/controllers/Login_controller.php
@@ -3,7 +3,7 @@ session_cache_limiter('private_no_expire');
 session_start();
 	class Login_controller extends CI_Controller{
 		
-	  public function index(){
+	 public function index(){
 		//session_destroy(); 
 	  $this->load->view('pages/login');
 	  
@@ -11,30 +11,20 @@ session_start();
 	 public function getData()
 	 { 
 	 
-	   /*if( isset($_SESSION["username"])){	
-			echo "set";
-			redirect('forgotpassword', 'refresh');
-		}else{
-			echo "not set";
-			redirect('getdata', 'refresh');
-		}*/
 		$this->load->model('login_model');
 		$data=array(
 			'username'=>$this->input->post('username'),
 			'password'=>$this->input->post('password'),
 		); 
-		$check=$this->login_model->autho($data);
-		if($check){
-			//echo "success"; 
-			
+	    $check=$this->login_model->autho($data);
+	    if($check){
 			$_SESSION["username"] = $data['username'] ;
 			$_SESSION["password"] = $data['password'];
-			echo "set";
+			
 			$this->load->view('pages/index');
-		
-		}else{
+	   }else{
 			$this->load->view('pages/error_one');
-		}		
+	   }		
 	 }
 	 public function forgotPassword(){
 		 $this->load->view('pages/forgotpassword');
