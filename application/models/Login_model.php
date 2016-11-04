ee0960921791ce8f9dbc58ce439003df2498208c
iamswapdev <swapnilv27012016@gmail.com>
2016-11-04 13:33:01 +0530

    Model fine code

--- application/models/Login_model.php
+++ application/models/Login_model.php
@@ -5,18 +5,12 @@
 			$this->load->database();
 		}
 		public function autho($data){
-<<<<<<< HEAD
-		 $query=$this->db->query('select * from admin where user="'.$data['username'].'" and password="'.$data['password'].'"');
-		 $rows = $query->num_rows();
-		 if($rows > 0){
-=======
 		 $this->db->where('username',$data['username']);
 		 $this->db->where('password',$data['password']);
 		 $data=$this->db->get('admin');
 		 
 		  $rows = $data->num_rows();
 		 if($rows == 1){
->>>>>>> origin/master
 			 return true;
 		 }else
 		 {
