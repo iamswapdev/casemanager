cbf3a01c6205f56101b0c33ee13c83815e8ea303
pranavghodake <pranavghodake308@gmail.com>
2016-11-04 12:42:19 +0530

    sidebar menu changed
    assigned base url/login_controller/page

--- application/views/pages/sidebar.php
+++ application/views/pages/sidebar.php
@@ -36,44 +36,44 @@
             <li>
                 <a href="#"><span class="nav-label">Admin</span><span class="fa arrow"></span> </a>
                 <ul class="nav nav-second-level">
-                    <li><a href="<?php echo base_url();?>">Admin Privileges</a></li>
+                    <li><a href="<?php echo base_url();?>login_controller/adminprivilege">Admin Privileges</a></li>
                 </ul>
             </li>
             <li>
                 <a href="#"><span class="nav-label">Master</span><span class="fa arrow"></span> </a>
                 <ul class="nav nav-second-level">
-                    <li><a href="panels.html">Data Entry</a></li>
+                    <li><a href="<?php echo base_url();?>login_controller/dataentry">Data Entry</a></li>
                 </ul>
             </li>
             <li>
                 <a href="#"><span class="nav-label">Search</span><span class="fa arrow"></span> </a>
                 <ul class="nav nav-second-level">
-                    <li><a href="panels.html">Search</a></li>
-                    <li><a href="panels.html">Advanced Search</a></li>
+                    <li><a href="<?php echo base_url();?>login_controller/search">Search</a></li>
+                    <li><a href="<?php echo base_url();?>login_controller/advancedsearch">Advanced Search</a></li>
                 </ul>
             </li>
              <li>
                 <a href="#"><span class="nav-label">Work Area</span><span class="fa arrow"></span> </a>
                 <ul class="nav nav-second-level">
-                    <li><a href="panels.html">Case Information</a></li>
-                    <li><a href="panels.html">Data Entry(Case Entry Only)</a></li>
-                    <li><a href="panels.html">File Insert</a></li>
-                    <li><a href="panels.html">WorkFlow Report</a></li>
-                    <li><a href="panels.html">Calendar</a></li>
+                    <li><a href="<?php echo base_url();?>login_controller/caseinformation">Case Information</a></li>
+                    <li><a href="<?php echo base_url();?>login_controller/dataentry_workarea">Data Entry(Case Entry Only)</a></li>
+                    <li><a href="<?php echo base_url();?>login_controller/fileinsert">File Insert</a></li>
+                    <li><a href="<?php echo base_url();?>login_controller/workflowreport">WorkFlow Report</a></li>
+                    <li><a href="<?php echo base_url();?>login_controller/calendar">Calendar</a></li>
                 </ul>
             </li>
             <li>
                 <a href="#"><span class="nav-label">WorkDesk</span><span class="fa arrow"></span> </a>
                 <ul class="nav nav-second-level">
-                    <li><a href="panels.html">WorkDesk</a></li>
+                    <li><a href="<?php echo base_url();?>login_controller/workdesk">WorkDesk</a></li>
                 </ul>
             </li>
              <li>
                 <a href="#"><span class="nav-label">Financials</span><span class="fa arrow"></span> </a>
                 <ul class="nav nav-second-level">
-                    <li><a href="panels.html">Financials</a></li>
-                    <li><a href="panels.html">Reports</a></li>
-                    <li><a href="panels.html">Rapid Funds</a></li>
+                    <li><a href="<?php echo base_url();?>login_controller/financials">Financials</a></li>
+                    <li><a href="<?php echo base_url();?>login_controller/reports">Reports</a></li>
+                    <li><a href="<?php echo base_url();?>login_controller/rapidfunds">Rapid Funds</a></li>
                 </ul>
             </li>
              <li>
