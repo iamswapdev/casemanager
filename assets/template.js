8ef727beaf42cbee9268d1fdba7c1f71ce93fb52
Abhijeet <abhijeettakmare12@gmail.com>
2017-02-18 15:04:25 +0530

    Removed only spaces from templates

--- assets/template.js
+++ assets/template.js
@@ -1,2 +1,6 @@
 $("span[style='mso-spacerun:yes']").html("");
 $("span[style='mso-tab-count:2']").html("");
+
+$("span[style='mso-tab-count:1']").html("");
+$("span[style='mso-tab-count:3']").html("&#9;&#9;&#9;&#9;&#9;&#9;&#9;");
+
