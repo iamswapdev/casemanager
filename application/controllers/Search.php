7ebe00071d99b7a66051d2ed500d8c9727ba59d8
Abhijeet <abhijeettakmare12@gmail.com>
2017-02-21 15:04:05 +0530

    Latest changes

--- application/controllers/Search.php
+++ application/controllers/Search.php
@@ -1170,23 +1170,7 @@ class Search extends CI_Controller{
 			$this->load->view('pages/login', $CurrentPage);
 		}
 	}
-	public function folderSize($dir){
-		$count_size = 0;
-		$count = 0;
-		$dir_array = scandir($dir);
-		  foreach($dir_array as $key=>$filename){
-			if($filename!=".." && $filename!="."){
-			   if(is_dir($dir."/".$filename)){
-				  $new_foldersize = $this->foldersize($dir."/".$filename);
-				  $count_size = $count_size+ $new_foldersize;
-				}else if(is_file($dir."/".$filename)){
-				  $count_size = $count_size + filesize($dir."/".$filename);
-				  $count++;
-				}
-		   }
-		 }
-		return $count_size;
-	}
+	
 	
 /*****************************************************************************************************************************************/
 }
